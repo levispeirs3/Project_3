db026b6c9a9d8b6edb05ac8234a3324d4ef4d63d
levispeirs3 <speirs03@byu.edu>
2025-04-12 17:05:33 -0600

    added indentation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let globalDataObject
+
 // Set Up
 fetch("https://sports.is120.ckearl.com")
     .then((response) => response.json())
@@ -7,25 +8,36 @@ fetch("https://sports.is120.ckearl.com")
         globalDataObject = dataObject["data"]
         completeSteps(globalDataObject);
     });
+
 function completeSteps(allLeagues) {
     // makeTeamList(allLeagues)
     // console.log(allLeagues)
+
     let dropdown = document.getElementById("league-dropdown");
+
     dropdown.addEventListener("change", () => {
         let selectedLeague = dropdown.value;
         makeTeamList(allLeagues, selectedLeague);
     });
+
     makeTeamList(allLeagues, dropdown.value);
 }
+
+
 function makeTeamList(dataObject, selectedLeague) {
     console.log("Available keys in dataObject:", Object.keys(dataObject));
+
     let teams = dataObject[selectedLeague]["teams"];
+
     console.log("Teams from", selectedLeague + ":", teams)
     let teamList = document.getElementById("team-list");
+
     teamList.innerHTML = "";
+
     teams.forEach(team => {
         let option = document.createElement("option");
         option.textContent = team.name;
         teamList.appendChild(option);
     });
+    
 }
